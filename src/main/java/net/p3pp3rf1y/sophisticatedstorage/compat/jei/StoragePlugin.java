ebd83c9a4563d53e935d6f8535db3c91823a60db
P3pp3rF1y <janecek@usertechnologies.com>
2022-02-14 14:34:26 +0100

    feat: ✨ Added JEI compat

---
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/compat/jei/StoragePlugin.java
@@ -0,0 +1,74 @@
+package net.p3pp3rf1y.sophisticatedstorage.compat.jei;
+
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.VanillaRecipeCategoryUid;
+import mezz.jei.api.gui.handlers.IGuiContainerHandler;
+import mezz.jei.api.helpers.IStackHelper;
+import mezz.jei.api.recipe.transfer.IRecipeTransferHandlerHelper;
+import mezz.jei.api.registration.IGuiHandlerRegistration;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
+import mezz.jei.api.registration.IRecipeTransferRegistration;
+import net.minecraft.client.renderer.Rect2i;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.p3pp3rf1y.sophisticatedcore.compat.jei.CraftingContainerRecipeTransferHandlerBase;
+import net.p3pp3rf1y.sophisticatedcore.compat.jei.StorageGhostIngredientHandler;
+import net.p3pp3rf1y.sophisticatedstorage.SophisticatedStorage;
+import net.p3pp3rf1y.sophisticatedstorage.client.gui.StorageScreen;
+import net.p3pp3rf1y.sophisticatedstorage.client.gui.StorageSettingsScreen;
+import net.p3pp3rf1y.sophisticatedstorage.common.gui.StorageContainerMenu;
+import net.p3pp3rf1y.sophisticatedstorage.init.ModItems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("unused")
+@JeiPlugin
+public class StoragePlugin implements IModPlugin {
+	@Override
+	public ResourceLocation getPluginUid() {
+		return new ResourceLocation(SophisticatedStorage.MOD_ID, "default");
+	}
+
+	@Override
+	public void registerGuiHandlers(IGuiHandlerRegistration registration) {
+		registration.addGuiContainerHandler(StorageScreen.class, new IGuiContainerHandler<>() {
+			@Override
+			public List<Rect2i> getGuiExtraAreas(StorageScreen gui) {
+				List<Rect2i> ret = new ArrayList<>();
+				gui.getUpgradeSlotsRectangle().ifPresent(ret::add);
+				ret.addAll(gui.getUpgradeSettingsControl().getTabRectangles());
+				gui.getSortButtonsRectangle().ifPresent(ret::add);
+				return ret;
+			}
+		});
+
+		registration.addGuiContainerHandler(StorageSettingsScreen.class, new IGuiContainerHandler<>() {
+			@Override
+			public List<Rect2i> getGuiExtraAreas(StorageSettingsScreen gui) {
+				return new ArrayList<>(gui.getSettingsTabControl().getTabRectangles());
+			}
+		});
+
+		registration.addGhostIngredientHandler(StorageScreen.class, new StorageGhostIngredientHandler<>());
+	}
+
+	@Override
+	public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+		registration.addRecipeCatalyst(new ItemStack(ModItems.CRAFTING_UPGRADE.get()), VanillaRecipeCategoryUid.CRAFTING);
+	}
+
+	@Override
+	public void registerRecipeTransferHandlers(IRecipeTransferRegistration registration) {
+		IRecipeTransferHandlerHelper handlerHelper = registration.getTransferHelper();
+		IStackHelper stackHelper = registration.getJeiHelpers().getStackHelper();
+		registration.addRecipeTransferHandler(new CraftingContainerRecipeTransferHandlerBase<StorageContainerMenu>(handlerHelper, stackHelper) {
+			@Override
+			public Class<StorageContainerMenu> getContainerClass() {
+				return StorageContainerMenu.class;
+			}
+		}, VanillaRecipeCategoryUid.CRAFTING);
+	}
+
+}
