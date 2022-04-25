df0f6832f17ed5fb82948e2bec118ba3e4b2eb12
P3pp3rF1y <p3pp3rf1y@gmail.com>
2022-04-25 18:20:54 +0200

    feat: ✨ Added shulker box and chest tags

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/data/BlockTagProvider.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/data/BlockTagProvider.java
@@ -3,6 +3,7 @@ package net.p3pp3rf1y.sophisticatedstorage.data;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.BlockTagsProvider;
 import net.minecraft.tags.BlockTags;
+import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.p3pp3rf1y.sophisticatedstorage.SophisticatedStorage;
 import net.p3pp3rf1y.sophisticatedstorage.init.ModBlocks;
@@ -28,5 +29,8 @@ public class BlockTagProvider extends BlockTagsProvider {
 				ModBlocks.CHEST.get(), ModBlocks.IRON_CHEST.get(), ModBlocks.GOLD_CHEST.get(), ModBlocks.DIAMOND_CHEST.get(), ModBlocks.NETHERITE_CHEST.get(),
 				ModBlocks.SHULKER_BOX.get(), ModBlocks.IRON_SHULKER_BOX.get(), ModBlocks.GOLD_SHULKER_BOX.get(), ModBlocks.DIAMOND_SHULKER_BOX.get(), ModBlocks.NETHERITE_SHULKER_BOX.get()
 		);
+		tag(Tags.Blocks.CHESTS).add(ModBlocks.CHEST.get(), ModBlocks.IRON_CHEST.get(), ModBlocks.GOLD_CHEST.get(), ModBlocks.DIAMOND_CHEST.get(), ModBlocks.NETHERITE_CHEST.get());
+		tag(Tags.Blocks.CHESTS_WOODEN).add(ModBlocks.CHEST.get(), ModBlocks.IRON_CHEST.get(), ModBlocks.GOLD_CHEST.get(), ModBlocks.DIAMOND_CHEST.get(), ModBlocks.NETHERITE_CHEST.get());
+		tag(Tags.Blocks.BARRELS).add(ModBlocks.BARREL.get(), ModBlocks.IRON_BARREL.get(), ModBlocks.GOLD_BARREL.get(), ModBlocks.DIAMOND_BARREL.get(), ModBlocks.NETHERITE_BARREL.get());
 	}
 }
