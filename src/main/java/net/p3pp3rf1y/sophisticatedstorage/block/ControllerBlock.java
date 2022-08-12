b6b4e4e94a613c3496c77fa8973c922a3116f50e
P3pp3rF1y <p3pp3rf1y@gmail.com>
2022-08-12 22:20:11 +0200

    feat: ✨ Added hand interaction to controller - single click puts the stack in hand if it already exists in one of the connected inventories, double click looks through player's inventory and any stack which already exists in one of the inventories is put into that storage.

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/block/ControllerBlock.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/block/ControllerBlock.java
@@ -1,7 +1,10 @@
 package net.p3pp3rf1y.sophisticatedstorage.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -9,6 +12,7 @@ import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
+import net.minecraft.world.phys.BlockHitResult;
 import net.p3pp3rf1y.sophisticatedcore.controller.ControllerBlockEntityBase;
 import net.p3pp3rf1y.sophisticatedcore.util.WorldHelper;
 import org.jetbrains.annotations.Nullable;
@@ -35,5 +39,17 @@ public class ControllerBlock extends Block implements EntityBlock {
 	public void setPlacedBy(Level pLevel, BlockPos pPos, BlockState pState, @Nullable LivingEntity pPlacer, ItemStack pStack) {
 		super.setPlacedBy(pLevel, pPos, pState, pPlacer, pStack);
 		WorldHelper.getBlockEntity(pLevel, pPos, ControllerBlockEntity.class).ifPresent(ControllerBlockEntityBase::searchAndAddStorages);
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
+		if (level.isClientSide()) {
+			return InteractionResult.SUCCESS;
+		}
+
+		WorldHelper.getBlockEntity(level, pos, ControllerBlockEntity.class).ifPresent(controller -> controller.depositPlayerItems(player, hand));
+
+		return InteractionResult.SUCCESS;
 	}
 }
