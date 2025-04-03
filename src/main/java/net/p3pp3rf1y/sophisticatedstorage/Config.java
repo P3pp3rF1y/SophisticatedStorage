1987ab46da882bd8eabffdcbdeb44d4c1c8b50fd
P3pp3rF1y <p3pp3rf1y@gmail.com>
2025-04-03 17:04:17 +0200

    feat: ✨ Added limitedBarrelCountDyeingEnabled config (by default set to true) that allows disabling the dye interaction with limited barrels that changes color of the counts shown

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/Config.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/Config.java
@@ -164,6 +164,7 @@ public class Config {
 
 		public final ModConfigSpec.IntValue tooManyItemEntityDrops;
 		public final MaxUgradesPerStorageConfig maxUpgradesPerStorage;
+		public final ModConfigSpec.BooleanValue limitedBarrelCountDyeingEnabled;
 
 		public void initListeners(IEventBus modBus) {
 			modBus.addListener(this::onConfigReload);
@@ -290,6 +291,7 @@ public class Config {
 			));
 
 			tooManyItemEntityDrops = builder.comment("Threshold of number of item entities dropped from chest / barrel above which break is canceled (unless shift key is pressed) and message is displayed explaining to player many drops and packing tape use").defineInRange("tooManyItemEntityDrops", 200, 0, 1000);
+			limitedBarrelCountDyeingEnabled = builder.comment("Determines if limited barrel counts can be dyed to change their color").define("limitedBarrelCountDyeingEnabled", true);
 			builder.pop();
 		}
 
