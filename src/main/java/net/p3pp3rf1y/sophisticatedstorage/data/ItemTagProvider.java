d168c10a7900ea93a1a94bf3572100d4d9d98a4e
P3pp3rF1y <p3pp3rf1y@gmail.com>
2025-04-22 21:25:32 +0200

    feat: ✨ Added item tags for barrels / chests and their wooden variants so that these can be easily used in recipes

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/data/ItemTagProvider.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/data/ItemTagProvider.java
@@ -5,6 +5,7 @@ import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.p3pp3rf1y.sophisticatedstorage.SophisticatedStorage;
@@ -28,5 +29,10 @@ public class ItemTagProvider extends ItemTagsProvider {
 
 		IntrinsicTagAppender<Item> allStorageTag = tag(ModBlocks.ALL_STORAGE_TAG);
 		ForgeRegistries.ITEMS.getEntries().stream().map(Map.Entry::getValue).filter(item -> item instanceof StorageBlockItem).forEach(allStorageTag::add);
+
+		tag(Tags.Items.CHESTS).add(ModBlocks.CHEST_ITEM.get(), ModBlocks.COPPER_CHEST_ITEM.get(), ModBlocks.IRON_CHEST_ITEM.get(), ModBlocks.GOLD_CHEST_ITEM.get(), ModBlocks.DIAMOND_CHEST_ITEM.get(), ModBlocks.NETHERITE_CHEST_ITEM.get());
+		tag(Tags.Items.CHESTS_WOODEN).add(ModBlocks.CHEST_ITEM.get());
+		tag(Tags.Items.BARRELS).add(ModBlocks.BARREL_ITEM.get(), ModBlocks.COPPER_BARREL_ITEM.get(), ModBlocks.IRON_BARREL_ITEM.get(), ModBlocks.GOLD_BARREL_ITEM.get(), ModBlocks.DIAMOND_BARREL_ITEM.get(), ModBlocks.NETHERITE_BARREL_ITEM.get());
+		tag(Tags.Items.BARRELS_WOODEN).add(ModBlocks.BARREL_ITEM.get());
 	}
 }
