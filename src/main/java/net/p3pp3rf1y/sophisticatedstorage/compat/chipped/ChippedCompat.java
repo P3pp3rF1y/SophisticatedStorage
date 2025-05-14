P3pp3rF1y/SophisticatedBackpacks

feat: Added EMI compatibility

src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/compat/chipped/ChippedCompat.java
@@ -1,6 +1,7 @@
 package net.p3pp3rf1y.sophisticatedbackpacks.compat.chipped;
 
 import earth.terrarium.chipped.common.compat.jei.WorkbenchCategory;
+import earth.terrarium.chipped.common.registry.ModBlocks;
 import earth.terrarium.chipped.common.registry.ModRecipeTypes;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.Item;
@@ -11,7 +12,9 @@ import net.neoforged.fml.loading.FMLEnvironment;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.RegisterEvent;
 import net.p3pp3rf1y.sophisticatedbackpacks.Config;
-import net.p3pp3rf1y.sophisticatedbackpacks.compat.jei.SBPPlugin;
+import net.p3pp3rf1y.sophisticatedbackpacks.SophisticatedBackpacks;
+import net.p3pp3rf1y.sophisticatedbackpacks.compat.recipeviewers.emi.EmiCompat;
+import net.p3pp3rf1y.sophisticatedbackpacks.compat.recipeviewers.jei.SBPPlugin;
 import net.p3pp3rf1y.sophisticatedbackpacks.init.ModItems;
 import net.p3pp3rf1y.sophisticatedcore.common.gui.UpgradeContainerRegistry;
 import net.p3pp3rf1y.sophisticatedcore.common.gui.UpgradeContainerType;
@@ -53,6 +56,17 @@ public class ChippedCompat implements ICompat {
 				registration.addRecipeCatalyst(new ItemStack(MASON_TABLE_UPGRADE.get()), WorkbenchCategory.RECIPE);
 				registration.addRecipeCatalyst(new ItemStack(ALCHEMY_BENCH_UPGRADE.get()), WorkbenchCategory.RECIPE);
 				registration.addRecipeCatalyst(new ItemStack(TINKERING_TABLE_UPGRADE.get()), WorkbenchCategory.RECIPE);
+			})).get().run();
+		}
+		if (ModList.get().isLoaded(CompatModIds.EMI)) {
+			((Supplier<Runnable>) () -> () -> EmiCompat.setAdditionalWorkstations(registration -> {
+				registration.addWorkstation(SophisticatedBackpacks.getRL("botanist_workbench"), ModBlocks.BOTANIST_WORKBENCH.get(), BOTANIST_WORKBENCH_UPGRADE.get());
+				registration.addWorkstation(SophisticatedBackpacks.getRL("glassblower"), ModBlocks.GLASSBLOWER.get(), GLASSBLOWER_UPGRADE.get());
+				registration.addWorkstation(SophisticatedBackpacks.getRL("carpenters_table"), ModBlocks.CARPENTERS_TABLE.get(), CARPENTERS_TABLE_UPGRADE.get());
+				registration.addWorkstation(SophisticatedBackpacks.getRL("loom_table"), ModBlocks.LOOM_TABLE.get(), LOOM_TABLE_UPGRADE.get());
+				registration.addWorkstation(SophisticatedBackpacks.getRL("mason_table"), ModBlocks.MASON_TABLE.get(), MASON_TABLE_UPGRADE.get());
+				registration.addWorkstation(SophisticatedBackpacks.getRL("alchemy_bench"), ModBlocks.ALCHEMY_BENCH.get(), ALCHEMY_BENCH_UPGRADE.get());
+				registration.addWorkstation(SophisticatedBackpacks.getRL("tinkering_table"), ModBlocks.TINKERING_TABLE.get(), TINKERING_TABLE_UPGRADE.get());
 			})).get().run();
 		}
 	}

src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/compat/sawmill/SawmillCompat.java
@@ -1,5 +1,6 @@
 package net.p3pp3rf1y.sophisticatedbackpacks.compat.sawmill;
 
+import net.mehvahdjukaar.sawmill.integration.emi.EMIPlugin;
 import net.mehvahdjukaar.sawmill.integration.jei.JEIPlugin;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.Item;
@@ -10,7 +11,8 @@ import net.neoforged.fml.loading.FMLEnvironment;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.RegisterEvent;
 import net.p3pp3rf1y.sophisticatedbackpacks.Config;
-import net.p3pp3rf1y.sophisticatedbackpacks.compat.jei.SBPPlugin;
+import net.p3pp3rf1y.sophisticatedbackpacks.compat.recipeviewers.emi.EmiCompat;
+import net.p3pp3rf1y.sophisticatedbackpacks.compat.recipeviewers.jei.SBPPlugin;
 import net.p3pp3rf1y.sophisticatedbackpacks.init.ModItems;
 import net.p3pp3rf1y.sophisticatedcore.common.gui.UpgradeContainerRegistry;
 import net.p3pp3rf1y.sophisticatedcore.common.gui.UpgradeContainerType;
@@ -33,6 +35,11 @@ public class SawmillCompat implements ICompat {
 		if (ModList.get().isLoaded(CompatModIds.JEI)) {
 			((Supplier<Runnable>) () -> () -> SBPPlugin.setAdditionalCatalystRegistrar(registration -> {
 				registration.addRecipeCatalyst(new ItemStack(SAWMILL_UPGRADE.get()), JEIPlugin.WOODCUTTING_RECIPE_TYPE);
+			})).get().run();
+		}
+		if (ModList.get().isLoaded(CompatModIds.EMI)) {
+			((Supplier<Runnable>) () -> () -> EmiCompat.setAdditionalWorkstations(registration -> {
+				registration.addWorkstation(EMIPlugin.WOODCUTTING_CATEGORY, SAWMILL_UPGRADE.get());
 			})).get().run();
 		}
 	}

P3pp3rF1y/SophisticatedStorage

src/main/java/net/p3pp3rf1y/sophisticatedstorage/compat/chipped/ChippedCompat.java
@@ -1,6 +1,7 @@
 package net.p3pp3rf1y.sophisticatedstorage.compat.chipped;
 
 import earth.terrarium.chipped.common.compat.jei.WorkbenchCategory;
+import earth.terrarium.chipped.common.registry.ModBlocks;
 import earth.terrarium.chipped.common.registry.ModRecipeTypes;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.Item;
@@ -18,7 +19,9 @@ import net.p3pp3rf1y.sophisticatedcore.compat.chipped.BlockTransformationUpgrade
 import net.p3pp3rf1y.sophisticatedcore.compat.chipped.BlockTransformationUpgradeItem;
 import net.p3pp3rf1y.sophisticatedcore.compat.chipped.BlockTransformationUpgradeWrapper;
 import net.p3pp3rf1y.sophisticatedstorage.Config;
-import net.p3pp3rf1y.sophisticatedstorage.compat.jei.StoragePlugin;
+import net.p3pp3rf1y.sophisticatedstorage.SophisticatedStorage;
+import net.p3pp3rf1y.sophisticatedstorage.compat.recipeviewers.emi.EmiCompat;
+import net.p3pp3rf1y.sophisticatedstorage.compat.recipeviewers.jei.StoragePlugin;
 import net.p3pp3rf1y.sophisticatedstorage.init.ModItems;
 
 import java.util.function.Supplier;
@@ -53,6 +56,18 @@ public class ChippedCompat implements ICompat {
 				registration.addRecipeCatalyst(new ItemStack(MASON_TABLE_UPGRADE.get()), WorkbenchCategory.RECIPE);
 				registration.addRecipeCatalyst(new ItemStack(ALCHEMY_BENCH_UPGRADE.get()), WorkbenchCategory.RECIPE);
 				registration.addRecipeCatalyst(new ItemStack(TINKERING_TABLE_UPGRADE.get()), WorkbenchCategory.RECIPE);
+			})).get().run();
+		}
+
+		if (ModList.get().isLoaded(CompatModIds.EMI)) {
+			((Supplier<Runnable>) () -> () -> EmiCompat.setAdditionalWorkstations(registration -> {
+				registration.addWorkstation(SophisticatedStorage.getRL("botanist_workbench"), ModBlocks.BOTANIST_WORKBENCH.get(), BOTANIST_WORKBENCH_UPGRADE.get());
+				registration.addWorkstation(SophisticatedStorage.getRL("glassblower"), ModBlocks.GLASSBLOWER.get(), GLASSBLOWER_UPGRADE.get());
+				registration.addWorkstation(SophisticatedStorage.getRL("carpenters_table"), ModBlocks.CARPENTERS_TABLE.get(), CARPENTERS_TABLE_UPGRADE.get());
+				registration.addWorkstation(SophisticatedStorage.getRL("loom_table"), ModBlocks.LOOM_TABLE.get(), LOOM_TABLE_UPGRADE.get());
+				registration.addWorkstation(SophisticatedStorage.getRL("mason_table"), ModBlocks.MASON_TABLE.get(), MASON_TABLE_UPGRADE.get());
+				registration.addWorkstation(SophisticatedStorage.getRL("alchemy_bench"), ModBlocks.ALCHEMY_BENCH.get(), ALCHEMY_BENCH_UPGRADE.get());
+				registration.addWorkstation(SophisticatedStorage.getRL("tinkering_table"), ModBlocks.TINKERING_TABLE.get(), TINKERING_TABLE_UPGRADE.get());
 			})).get().run();
 		}
 	}

src/main/java/net/p3pp3rf1y/sophisticatedstorage/compat/sawmill/SawmillCompat.java
@@ -1,5 +1,6 @@
 package net.p3pp3rf1y.sophisticatedstorage.compat.sawmill;
 
+import net.mehvahdjukaar.sawmill.integration.emi.EMIPlugin;
 import net.mehvahdjukaar.sawmill.integration.jei.JEIPlugin;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.Item;
@@ -16,7 +17,8 @@ import net.p3pp3rf1y.sophisticatedcore.compat.ICompat;
 import net.p3pp3rf1y.sophisticatedcore.compat.sawmill.SawmillUpgradeContainer;
 import net.p3pp3rf1y.sophisticatedcore.compat.sawmill.SawmillUpgradeItem;
 import net.p3pp3rf1y.sophisticatedstorage.Config;
-import net.p3pp3rf1y.sophisticatedstorage.compat.jei.StoragePlugin;
+import net.p3pp3rf1y.sophisticatedstorage.compat.recipeviewers.emi.EmiCompat;
+import net.p3pp3rf1y.sophisticatedstorage.compat.recipeviewers.jei.StoragePlugin;
 import net.p3pp3rf1y.sophisticatedstorage.init.ModItems;
 
 import java.util.function.Supplier;
@@ -33,6 +35,11 @@ public class SawmillCompat implements ICompat {
 		if (ModList.get().isLoaded(CompatModIds.JEI)) {
 			((Supplier<Runnable>) () -> () -> StoragePlugin.setAdditionalCatalystRegistrar(registration -> {
 				registration.addRecipeCatalyst(new ItemStack(SAWMILL_UPGRADE.get()), JEIPlugin.WOODCUTTING_RECIPE_TYPE);
+			})).get().run();
+		}
+		if (ModList.get().isLoaded(CompatModIds.EMI)) {
+			((Supplier<Runnable>) () -> () -> EmiCompat.setAdditionalWorkstations(registration -> {
+				registration.addWorkstation(EMIPlugin.WOODCUTTING_CATEGORY, SAWMILL_UPGRADE.get());
 			})).get().run();
 		}
 	}