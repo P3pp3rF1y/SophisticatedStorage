82706f7519cf3b3f19e6a4929afa01d8babc7e43
P3pp3rF1y <p3pp3rf1y@gmail.com>
2025-08-21 20:38:27 +0200

    fix: 🐛 Fixed wooden storage blocks when dropPacked setting is switched on to not drop dupe of their contents in addition to dropping packed with the same contents. Also fixed to not pack when there are no items in their inventory and no upgrades in upgrade slots - just drops as regular item.

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/block/WoodStorageBlockBase.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/block/WoodStorageBlockBase.java
@@ -68,7 +68,11 @@ public abstract class WoodStorageBlockBase extends StorageBlockBase implements I
 	@Override
 	public void onBlockExploded(BlockState state, Level level, BlockPos pos, Explosion explosion) {
 		if (Boolean.TRUE.equals(Config.COMMON.dropPacked.get())) {
-			WorldHelper.getBlockEntity(level, pos, WoodStorageBlockEntity.class).ifPresent(wbe -> wbe.setPacked(true));
+			WorldHelper.getBlockEntity(level, pos, WoodStorageBlockEntity.class).ifPresent(wbe -> {
+				if (isNonEmpty(wbe)) {
+					wbe.setPacked(true);
+				}
+			});
 		}
 		super.onBlockExploded(state, level, pos, explosion);
 	}
@@ -77,6 +81,7 @@ public abstract class WoodStorageBlockBase extends StorageBlockBase implements I
 		if (be instanceof WoodStorageBlockEntity wbe) {
 			addNameWoodAndTintData(stack, wbe);
 			if (wbe.isPacked() || shouldNonEmptyDropPacked(wbe)) {
+				wbe.setPacked(true);
 				StorageWrapper storageWrapper = be.getStorageWrapper();
 				UUID storageUuid = storageWrapper.getContentsUuid().orElse(UUID.randomUUID());
 				CompoundTag storageContents = wbe.getStorageContentsTag();
@@ -97,6 +102,10 @@ public abstract class WoodStorageBlockBase extends StorageBlockBase implements I
 			return false;
 		}
 
+		return isNonEmpty(wbe);
+	}
+
+	private static boolean isNonEmpty(WoodStorageBlockEntity wbe) {
 		return !InventoryHelper.isEmpty(wbe.getStorageWrapper().getInventoryHandler()) || !InventoryHelper.isEmpty(wbe.getStorageWrapper().getUpgradeHandler());
 	}
 
@@ -210,7 +219,7 @@ public abstract class WoodStorageBlockBase extends StorageBlockBase implements I
 		BlockState ret = super.playerWillDestroy(level, pos, state, player);
 		WorldHelper.getBlockEntity(level, pos, WoodStorageBlockEntity.class)
 				.ifPresent(wbe -> {
-					if (Boolean.TRUE.equals(Config.COMMON.dropPacked.get())) {
+					if (Boolean.TRUE.equals(Config.COMMON.dropPacked.get()) && isNonEmpty(wbe)) {
 						wbe.setPacked(true);
 					}
 
