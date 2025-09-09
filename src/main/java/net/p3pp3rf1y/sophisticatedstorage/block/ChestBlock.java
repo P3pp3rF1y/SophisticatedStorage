846793f45dbf5916dc1c5b998b5f813e55d6652e
P3pp3rF1y <p3pp3rf1y@gmail.com>
2025-09-09 10:33:05 +0200

    fix: 🐛 Fixed double chests to properly drop with their contents when dropPacked is set to true in config and right part of the double chest is broken

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/block/ChestBlock.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/block/ChestBlock.java
@@ -349,7 +349,7 @@ public class ChestBlock extends WoodStorageBlockBase implements SimpleWaterlogge
 	}
 
 	@Override
-	public boolean onDestroyedByPlayer(BlockState state, Level level, BlockPos pos, Player player, boolean willHarvest, FluidState fluid) {
+	public BlockState playerWillDestroy(Level level, BlockPos pos, BlockState state, Player player) {
 		if (state.getValue(TYPE) != ChestType.SINGLE) {
 			level.getBlockEntity(pos, ModBlocks.CHEST_BLOCK_ENTITY_TYPE.get()).ifPresent(be -> {
 				be.setDestroyedByPlayer();
@@ -368,8 +368,7 @@ public class ChestBlock extends WoodStorageBlockBase implements SimpleWaterlogge
 				}
 			});
 		}
-
-		return super.onDestroyedByPlayer(state, level, pos, player, willHarvest, fluid);
+		return super.playerWillDestroy(level, pos, state, player);
 	}
 
 	@Override
