6c755f55b19ef5a5416f7e1bf504cb7203de2a3d
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-01-04 12:31:35 +0100

    fix: 🐛 Fixed double chest to form when second chest is placed even if the first one already has any upgrades

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/block/ChestBlock.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/block/ChestBlock.java
@@ -194,7 +194,7 @@ public class ChestBlock extends WoodStorageBlockBase implements SimpleWaterlogge
 				StorageBlockItem.getMainColorFromComponentHolder(chestBeingPlaced).orElse(-1),
 				StorageBlockItem.getAccentColorFromComponentHolder(chestBeingPlaced).orElse(-1),
 				WoodStorageBlockItem.getWoodType(chestBeingPlaced).orElse(WoodType.ACACIA),
-				wrapper.hasContents() && InventoryHelper.isEmpty(wrapper.getUpgradeHandler()));
+				!wrapper.hasContents() || InventoryHelper.isEmpty(wrapper.getUpgradeHandler()));
 	}
 
 	private BlockState getStateForPlacement(BlockPlaceContext context, Direction direction, FluidState fluidstate, int mainColor, int accentColor, WoodType woodType, boolean itemHasNoUpgrades) {
@@ -332,15 +332,21 @@ public class ChestBlock extends WoodStorageBlockBase implements SimpleWaterlogge
 
 	private static void joinChests(LevelAccessor level, BlockPos pos, BlockPos otherPos, ChestType currentChestType) {
 		level.getBlockEntity(pos, ModBlocks.CHEST_BLOCK_ENTITY_TYPE.get()).ifPresent(currentBE ->
-				joinWithChest(level, otherPos, currentChestType, currentBE)
+						level.getBlockEntity(otherPos, ModBlocks.CHEST_BLOCK_ENTITY_TYPE.get()).ifPresent(
+								otherChest -> {
+									if (!InventoryHelper.isEmpty(currentBE.getStorageWrapper().getUpgradeHandler()) && !InventoryHelper.isEmpty(otherChest.getStorageWrapper().getUpgradeHandler())) {
+										return;
+									}
+									joinWithChest(level, otherPos, currentChestType, currentBE);
+								}
+						)
 		);
 	}
 
 	private static void joinWithChest(LevelReader level, BlockPos otherPos, ChestType currentChestType, ChestBlockEntity currentBE) {
 		level.getBlockEntity(otherPos, ModBlocks.CHEST_BLOCK_ENTITY_TYPE.get())
 				.ifPresent(otherBE -> {
-					if (InventoryHelper.isEmpty(currentBE.getStorageWrapper().getUpgradeHandler())
-							&& (currentChestType == ChestType.LEFT || !InventoryHelper.isEmpty(otherBE.getStorageWrapper().getUpgradeHandler()))) {
+					if (currentChestType == ChestType.LEFT) {
 						currentBE.joinWithChest(otherBE);
 						currentBE.syncTogglesFrom(otherBE);
 					} else {
