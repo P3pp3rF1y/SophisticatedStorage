f492cbf006c55baa250e356eca206d1fff6088cd
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-01-26 17:42:17 +0100

    feat: ✨ Updated limited barrels to properly work with the new Void upgrade functionality

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/block/LimitedBarrelBlockEntity.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/block/LimitedBarrelBlockEntity.java
@@ -18,6 +18,7 @@ import net.p3pp3rf1y.sophisticatedcore.settings.SettingsHandler;
 import net.p3pp3rf1y.sophisticatedcore.settings.itemdisplay.ItemDisplaySettingsCategory;
 import net.p3pp3rf1y.sophisticatedcore.settings.memory.MemorySettingsCategory;
 import net.p3pp3rf1y.sophisticatedcore.settings.nosort.NoSortSettingsCategory;
+import net.p3pp3rf1y.sophisticatedcore.upgrades.voiding.VoidType;
 import net.p3pp3rf1y.sophisticatedcore.upgrades.voiding.VoidUpgradeWrapper;
 import net.p3pp3rf1y.sophisticatedcore.util.InventoryHelper;
 import net.p3pp3rf1y.sophisticatedcore.util.NBTHelper;
@@ -36,7 +37,7 @@ import java.util.function.Predicate;
 public class LimitedBarrelBlockEntity extends BarrelBlockEntity implements ICountDisplay, IFillLevelDisplay {
 	public static final Consumer<VoidUpgradeWrapper> VOID_UPGRADE_VOIDING_OVERFLOW_OF_EVERYTHING_BY_DEFAULT = voidUpgrade -> {
 		voidUpgrade.getFilterLogic().setAllowByDefault(false);
-		voidUpgrade.setShouldVoidOverflowDefaultOrLoadFromNbt(true);
+		voidUpgrade.setVoidOverflowDefaultOrLoadFromNbt(VoidType.SLOT_OVERFLOW);
 	};
 	public static final String STORAGE_TYPE = "limited_barrel";
 	private long lastDepositTime = -100;
