246de9a6d235aa6c80b1da3d474a8017676d91f2
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-04-01 11:16:50 +0200

    fix: 🐛 Fixed double chests to prevent their halfs from being rotated by wrenches and other modded tools

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/block/ChestBlock.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/block/ChestBlock.java
@@ -430,6 +430,9 @@ public class ChestBlock extends WoodStorageBlockBase implements SimpleWaterlogge
 	@SuppressWarnings("deprecation")
 	@Override
 	public BlockState mirror(BlockState state, Mirror mirror) {
+		if (state.getValue(TYPE) != ChestType.SINGLE) {
+			return state;
+		}
 		return state.rotate(mirror.getRotation(state.getValue(FACING)));
 	}
 
@@ -516,6 +519,9 @@ public class ChestBlock extends WoodStorageBlockBase implements SimpleWaterlogge
 
 	@Override
 	public BlockState rotate(BlockState state, Rotation rotation) {
+		if (state.getValue(TYPE) != ChestType.SINGLE) {
+			return state;
+		}
 		return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
 	}
 
