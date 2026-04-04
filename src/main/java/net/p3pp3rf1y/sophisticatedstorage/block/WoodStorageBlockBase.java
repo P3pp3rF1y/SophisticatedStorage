15a0a668e7c131c3e8ffadc01b24c252609b6dcc
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-04-04 02:27:44 +0200

    fix: 🐛 Fixed storage clone and packed render side effects

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/block/WoodStorageBlockBase.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/block/WoodStorageBlockBase.java
@@ -81,8 +81,8 @@ public abstract class WoodStorageBlockBase extends StorageBlockBase implements I
 	public void addDropData(ItemStack stack, StorageBlockEntity be) {
 		if (be instanceof WoodStorageBlockEntity wbe) {
 			addNameWoodAndTintData(stack, wbe);
-			if (wbe.isPacked() || shouldNonEmptyDropPacked(wbe)) {
-				wbe.setPacked(true);
+			boolean packed = wbe.isPacked() || shouldNonEmptyDropPacked(wbe);
+			if (packed) {
 				StorageWrapper storageWrapper = be.getStorageWrapper();
 				UUID storageUuid = storageWrapper.getContentsUuid().orElse(UUID.randomUUID());
 				CompoundTag storageContents = wbe.getStorageContentsTag();
@@ -162,7 +162,13 @@ public abstract class WoodStorageBlockBase extends StorageBlockBase implements I
 	@Override
 	public ItemStack getCloneItemStack(BlockState state, HitResult target, LevelReader level, BlockPos pos, Player player) {
 		ItemStack stack = new ItemStack(this);
-		addNameWoodAndTintData(stack, level, pos);
+		WorldHelper.getBlockEntity(level, pos, WoodStorageBlockEntity.class).ifPresentOrElse(be -> {
+			if (be.isPacked()) {
+				addDropData(stack, be);
+			} else {
+				addNameWoodAndTintData(stack, be);
+			}
+		}, () -> addNameWoodAndTintData(stack, level, pos));
 		return stack;
 	}
 
@@ -213,6 +219,8 @@ public abstract class WoodStorageBlockBase extends StorageBlockBase implements I
 	protected void setRenderBlockRenderProperties(ItemStack stack, WoodStorageBlockEntity be) {
 		WoodStorageBlockItem.getWoodType(stack).ifPresent(be::setWoodType);
 		be.getStorageWrapper().setColors(StorageBlockItem.getMainColorFromComponentHolder(stack).orElse(-1), StorageBlockItem.getAccentColorFromComponentHolder(stack).orElse(-1));
+		be.setUpdateBlockRender();
+		WorldHelper.notifyBlockUpdate(be);
 	}
 
 	@Override
