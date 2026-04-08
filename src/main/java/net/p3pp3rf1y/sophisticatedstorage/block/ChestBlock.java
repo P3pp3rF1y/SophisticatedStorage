6f0fb1dc510b6b8052ee5fe7d64f633b7e038756
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-04-09 00:49:31 +0200

    feat: ✨ Added grouped storage highlight positions

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/block/ChestBlock.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/block/ChestBlock.java
@@ -35,7 +35,6 @@ import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
-import net.p3pp3rf1y.sophisticatedcore.util.IDoubleBlock;
 import net.p3pp3rf1y.sophisticatedcore.util.InventoryHelper;
 import net.p3pp3rf1y.sophisticatedcore.util.WorldHelper;
 import net.p3pp3rf1y.sophisticatedstorage.Config;
@@ -52,7 +51,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.function.Supplier;
 
-public class ChestBlock extends WoodStorageBlockBase implements SimpleWaterloggedBlock, IDoubleBlock {
+public class ChestBlock extends WoodStorageBlockBase implements SimpleWaterloggedBlock {
 	public static final EnumProperty<Direction> FACING = HorizontalDirectionalBlock.FACING;
 	public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
 	public static final EnumProperty<ChestType> TYPE = BlockStateProperties.CHEST_TYPE;
@@ -461,11 +460,6 @@ public class ChestBlock extends WoodStorageBlockBase implements SimpleWaterlogge
 			}
 			return List.of(origin.relative(facing), origin.relative(connectedDirection).relative(facing));
 		}
-	}
-
-	@Override
-	public Optional<BlockPos> getOtherPosition(BlockState state, BlockPos pos) {
-		return state.getValue(TYPE) == ChestType.SINGLE ? Optional.empty() : Optional.of(pos.relative(getConnectedDirection(state)));
 	}
 
 	@Override
