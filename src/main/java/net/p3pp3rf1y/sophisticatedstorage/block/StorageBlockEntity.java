d8e73aa34bae0f1da0a5c955405d3bfc867e0a40
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-04-12 14:42:44 +0200

    feat: ✨ Added render data validation to prevent incorrect particles

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/block/StorageBlockEntity.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/block/StorageBlockEntity.java
@@ -296,7 +296,7 @@ public abstract class StorageBlockEntity extends BlockEntity implements IControl
 	@Override
 	public void onLoad() {
 		super.onLoad();
-		storageWrapper.onInit();
+		storageWrapper.onInit(level);
 		registerWithControllerOnLoad();
 	}
 
