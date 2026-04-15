6c444e2ec829571ef18d407f2cb6d233da6950f1
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-04-15 23:48:50 +0200

    fix: 🐛 Fixed storage upgrade initialization recursion

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/block/StorageBlockEntity.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/block/StorageBlockEntity.java
@@ -610,6 +610,9 @@ public abstract class StorageBlockEntity extends BlockEntity implements IControl
 		if (direction == null) {
 			return;
 		}
+		if (storageWrapper.isUpgradeHandlerInitializing()) {
+			return;
+		}
 		storageWrapper.getUpgradeHandler().getWrappersThatImplement(INeighborChangeListenerUpgrade.class).forEach(upgrade -> upgrade.onNeighborChange(level, worldPosition, direction));
 	}
 
