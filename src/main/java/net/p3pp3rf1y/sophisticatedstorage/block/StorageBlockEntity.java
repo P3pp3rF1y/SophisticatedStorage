0a93c516a4efcd1e68f555f7a2f8adfaa2d12889
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-04-21 22:16:42 +0200

    feat: ✨ Added Clearable support to storage block entities

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/block/StorageBlockEntity.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/block/StorageBlockEntity.java
@@ -12,6 +12,7 @@ import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.Clearable;
 import net.minecraft.world.Nameable;
 import net.minecraft.world.entity.ContainerUser;
 import net.minecraft.world.entity.player.Player;
@@ -42,7 +43,7 @@ import net.p3pp3rf1y.sophisticatedstorage.upgrades.INeighborChangeListenerUpgrad
 import javax.annotation.Nullable;
 import java.util.*;
 
-public abstract class StorageBlockEntity extends BlockEntity implements IControllableStorage, ILinkable, ILockable, Nameable, ITierDisplay, IUpgradeDisplay {
+public abstract class StorageBlockEntity extends BlockEntity implements IControllableStorage, ILinkable, ILockable, Nameable, ITierDisplay, IUpgradeDisplay, Clearable {
 	public static final String STORAGE_WRAPPER = "storageWrapper";
 	public static final String UPDATE_BLOCK_RENDER_TAG = "updateBlockRender";
 	private final StorageWrapper storageWrapper;
@@ -409,6 +410,18 @@ public abstract class StorageBlockEntity extends BlockEntity implements IControl
 
 		InventoryHelper.dropResources(storageWrapper.getUpgradeHandler(), level, worldPosition);
 		isDroppingContents = false;
+	}
+
+	@Override
+	public void clearContent() {
+		for (int slot = 0; slot < storageWrapper.getInventoryHandler().size(); slot++) {
+			storageWrapper.getInventoryHandler().setStackInSlot(slot, ItemStack.EMPTY);
+		}
+		for (int slot = 0; slot < storageWrapper.getUpgradeHandler().size(); slot++) {
+			storageWrapper.getUpgradeHandler().setStackInSlot(slot, ItemStack.EMPTY);
+		}
+		setChanged();
+		invalidateCapabilitiesAndControllerCache();
 	}
 
 	public void setCustomName(Component customName) {
