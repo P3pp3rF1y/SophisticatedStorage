c9b473b9cc52ac9b98fd8604c98fd29f704455d7
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-04-29 23:14:34 +0200

    chore: 🔧 Use mod version for payload registration

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/SophisticatedStorage.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/SophisticatedStorage.java
@@ -21,10 +21,12 @@ import org.apache.logging.log4j.Logger;
 public class SophisticatedStorage {
 	public static final String MOD_ID = "sophisticatedstorage";
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+	private static String networkProtocolVersion;
 	private final CommonEventHandler commonEventHandler = new CommonEventHandler();
 
 	@SuppressWarnings("java:S1118") //needs to be public for mod to work
 	public SophisticatedStorage(IEventBus modBus, Dist dist, ModContainer container) {
+		networkProtocolVersion = container.getModInfo().getVersion().toString();
 		container.registerConfig(ModConfig.Type.SERVER, Config.SERVER_SPEC);
 		container.registerConfig(ModConfig.Type.CLIENT, Config.CLIENT_SPEC);
 		container.registerConfig(ModConfig.Type.COMMON, Config.COMMON_SPEC);
@@ -56,5 +58,9 @@ public class SophisticatedStorage {
 
 	public static String getRegistryName(String regName) {
 		return MOD_ID + ":" + regName;
+	}
+
+	public static String getNetworkProtocolVersion() {
+		return networkProtocolVersion;
 	}
 }
