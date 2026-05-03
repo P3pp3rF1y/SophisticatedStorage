bff763301f377971aa394d450e66e1f0274fd2a2
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-05-03 21:21:17 +0200

    feat: ✨ Added refill functionality to stonecutter, sawmill and chipped upgrades

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/compat/sawmill/SawmillCompatClient.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/compat/sawmill/SawmillCompatClient.java
@@ -1,6 +1,5 @@
 package net.p3pp3rf1y.sophisticatedstorage.compat.sawmill;
 
-import net.p3pp3rf1y.sophisticatedbackpacks.client.gui.SBPButtonDefinitions;
 import net.p3pp3rf1y.sophisticatedcore.client.gui.StorageScreenBase;
 import net.p3pp3rf1y.sophisticatedcore.client.gui.UpgradeGuiManager;
 import net.p3pp3rf1y.sophisticatedcore.client.gui.utils.Position;
@@ -8,12 +7,13 @@ import net.p3pp3rf1y.sophisticatedcore.common.gui.UpgradeContainerType;
 import net.p3pp3rf1y.sophisticatedcore.compat.sawmill.SawmillUpgradeContainer;
 import net.p3pp3rf1y.sophisticatedcore.compat.sawmill.SawmillUpgradeItem;
 import net.p3pp3rf1y.sophisticatedcore.compat.sawmill.SawmillUpgradeTab;
+import net.p3pp3rf1y.sophisticatedstorage.client.gui.StorageButtonDefinitions;
 
 public class SawmillCompatClient {
 	private SawmillCompatClient() {
 	}
 
 	public static void registerUpgradeTab(UpgradeContainerType<SawmillUpgradeItem.Wrapper, SawmillUpgradeContainer> containerType) {
-		UpgradeGuiManager.registerTab(containerType, (SawmillUpgradeContainer upgradeContainer, Position position, StorageScreenBase<?> screen) -> new SawmillUpgradeTab(upgradeContainer, position, screen, SBPButtonDefinitions.SHIFT_CLICK_TARGET));
+		UpgradeGuiManager.registerTab(containerType, (SawmillUpgradeContainer upgradeContainer, Position position, StorageScreenBase<?> screen) -> new SawmillUpgradeTab(upgradeContainer, position, screen, StorageButtonDefinitions.SHIFT_CLICK_TARGET, StorageButtonDefinitions.REFILL_INPUT));
 	}
 }
