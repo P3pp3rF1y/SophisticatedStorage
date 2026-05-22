113178d189e7470d6e476ddbc4638f96c18786e3
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-05-22 20:40:52 +0200

    fix: 🐛 Fixed dupe bug in shulkebox crafting recipe

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/crafting/ShulkerBoxFromVanillaShapelessRecipe.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/crafting/ShulkerBoxFromVanillaShapelessRecipe.java
@@ -47,7 +47,7 @@ public class ShulkerBoxFromVanillaShapelessRecipe extends ShapelessRecipe implem
 				wrapper.ensureContentsUuid();
 				InventoryHelper.iterate(itemCap, (slot, stack) -> {
 					if (!stack.isEmpty()) {
-						wrapper.getInventoryHandler().insertItem(stack, false);
+						wrapper.getInventoryHandler().insertItem(stack.copy(), false);
 					}
 				});
 			}
