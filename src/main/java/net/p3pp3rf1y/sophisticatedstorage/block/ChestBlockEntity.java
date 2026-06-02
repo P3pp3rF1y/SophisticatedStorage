af6c33df40f348da1a96bfdd816c52f3636281f9
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-06-02 10:16:51 +0200

    fix: 🐛 Fixed controller registration of double chests

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/block/ChestBlockEntity.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/block/ChestBlockEntity.java
@@ -243,6 +243,11 @@ public class ChestBlockEntity extends WoodStorageBlockEntity {
 	}
 
 	@Override
+	public BlockPos getControlledStorageBlockPos() {
+		return getMainPos();
+	}
+
+	@Override
 	public SophisticatedOpenersCounter getOpenersCounter() {
 		return openersCounter;
 	}
