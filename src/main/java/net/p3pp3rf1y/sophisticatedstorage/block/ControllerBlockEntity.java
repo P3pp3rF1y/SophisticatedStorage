2518a9944e2c6b2d587b0a443f3a5e7729b6dc3f
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-06-14 20:34:47 +0200

    feat: ✨ Updated controller deposits to match filtered storages

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/block/ControllerBlockEntity.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/block/ControllerBlockEntity.java
@@ -65,7 +65,7 @@ public class ControllerBlockEntity extends ControllerBlockEntityBase implements 
 	}
 
 	private boolean canDepositStack(ItemStack stack) {
-		return hasItem(stack.getItem()) || isMemorizedItem(stack) || isFilterItem(stack.getItem());
+		return hasItem(stack.getItem()) || isMemorizedItem(stack) || isFilterItem(stack.getItem()) || hasMatchingFilter(stack);
 	}
 
 	@Override
