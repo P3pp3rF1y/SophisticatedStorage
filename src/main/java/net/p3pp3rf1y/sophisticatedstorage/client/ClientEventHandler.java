666632a58bac9c37bbaf83aa5162279f0215367b
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-06-26 09:17:25 +0200

    feat: ✨ Updated paintbrush block outlines

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/client/ClientEventHandler.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/client/ClientEventHandler.java
@@ -1,6 +1,5 @@
 package net.p3pp3rf1y.sophisticatedstorage.client;
 
-import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.geom.ModelLayerLocation;
@@ -29,6 +28,8 @@ import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.p3pp3rf1y.sophisticatedcore.client.render.BlockHighlightRenderHelper;
+import net.p3pp3rf1y.sophisticatedcore.util.VoxelOutliner;
 import net.p3pp3rf1y.sophisticatedstorage.SophisticatedStorage;
 import net.p3pp3rf1y.sophisticatedstorage.block.ISimpleMaterialHolder;
 import net.p3pp3rf1y.sophisticatedstorage.block.LimitedBarrelBlock;
@@ -60,6 +61,8 @@ public class ClientEventHandler {
 	public static final ModelLayerLocation CHEST_LAYER = new ModelLayerLocation(CHEST_RL, "main");
 	public static final ModelLayerLocation CHEST_LEFT_LAYER = new ModelLayerLocation(CHEST_LEFT_RL, "main");
 	public static final ModelLayerLocation CHEST_RIGHT_LAYER = new ModelLayerLocation(CHEST_RIGHT_RL, "main");
+	private static final int PAINTBRUSH_CAN_APPLY_HIGHLIGHT_COLOR = 0x69c53b;
+	private static final int PAINTBRUSH_MISSING_ITEMS_HIGHLIGHT_COLOR = 0xc53b3b;
 
 	public static void registerHandlers() {
 		IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -120,13 +123,13 @@ public class ClientEventHandler {
 			if (blockState.getBlock() instanceof StorageBlockBase || blockState.getBlock() == ModBlocks.CONTROLLER.get()
 					|| level.getBlockEntity(pos) instanceof ISimpleMaterialHolder) {
 				PaintbrushOverlay.getItemRequirementsFor(stack, player, level, pos).ifPresent(itemRequirements -> {
-					float red = !itemRequirements.itemsMissing().isEmpty() ? 1 : 0;
-					float green = itemRequirements.itemsMissing().isEmpty() ? 1 : 0;
-					VertexConsumer vertexConsumer = event.getMultiBufferSource().getBuffer(RenderType.lines());
+					int color = itemRequirements.itemsMissing().isEmpty() ? PAINTBRUSH_CAN_APPLY_HIGHLIGHT_COLOR : PAINTBRUSH_MISSING_ITEMS_HIGHLIGHT_COLOR;
 					Vec3 cameraPos = event.getCamera().getPosition();
-					PoseStack poseStack = event.getPoseStack();
-					LevelRenderer.renderShape(poseStack, vertexConsumer, blockState.getShape(level, pos, CollisionContext.of(event.getCamera().getEntity())),
-							pos.getX() - cameraPos.x, pos.getY() - cameraPos.y, pos.getZ() - cameraPos.z, red, green, 0.0F, 1);
+					event.getPoseStack().pushPose();
+					event.getPoseStack().translate(pos.getX() - cameraPos.x, pos.getY() - cameraPos.y, pos.getZ() - cameraPos.z);
+					BlockHighlightRenderHelper.renderThickEdges(event.getPoseStack(), event.getMultiBufferSource(), color, VoxelOutliner
+							.linesFromVoxelShapeSimplified(blockState.getShape(level, pos, CollisionContext.of(event.getCamera().getEntity())), pos), pos);
+					event.getPoseStack().popPose();
 					event.setCanceled(true);
 				});
 			}
