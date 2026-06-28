89fcfa1f4874033502f084080e1ef0f1d8ecada2
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-06-28 09:37:18 +0200

    fix: 🐛 Fixed decoration table preview rotation animation stutter

---
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/client/render/DecorationTablePreviewRenderer.java
@@ -0,0 +1,39 @@
+package net.p3pp3rf1y.sophisticatedstorage.client.render;
+
+import com.mojang.blaze3d.platform.Lighting;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.render.pip.PictureInPictureRenderer;
+import net.minecraft.client.renderer.SubmitNodeCollector;
+import net.minecraft.client.renderer.item.TrackingItemStackRenderState;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+
+public class DecorationTablePreviewRenderer extends PictureInPictureRenderer<DecorationTablePreviewRenderState> {
+	@Override
+	public Class<DecorationTablePreviewRenderState> getRenderStateClass() {
+		return DecorationTablePreviewRenderState.class;
+	}
+
+	@Override
+	protected void renderToTexture(DecorationTablePreviewRenderState renderState, PoseStack poseStack, SubmitNodeCollector submitNodeCollector) {
+		poseStack.scale(1.0F, -1.0F, -1.0F);
+		TrackingItemStackRenderState itemStackRenderState = renderState.itemStackRenderState();
+		Minecraft.getInstance().gameRenderer.lighting().setupFor(itemStackRenderState.usesBlockLight() ? Lighting.Entry.ITEMS_3D : Lighting.Entry.ITEMS_FLAT);
+		itemStackRenderState.submit(poseStack, submitNodeCollector, 15728880, OverlayTexture.NO_OVERLAY, 0);
+	}
+
+	@Override
+	protected boolean textureIsReadyToBlit(DecorationTablePreviewRenderState renderState) {
+		return false;
+	}
+
+	@Override
+	protected float getTranslateY(int height, int guiScale) {
+		return height / 2.0F;
+	}
+
+	@Override
+	protected String getTextureLabel() {
+		return "decoration_table_preview";
+	}
+}
