f6c5440d984afe6d04354af5d4b17617ddeb9b0b
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-07-07 21:25:33 +0200

    fix: 🐛 Fixed packed storage cleaning to take precedence over placement

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/init/ModBlocks.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/init/ModBlocks.java
@@ -562,10 +562,6 @@ public class ModBlocks {
 			}
 		}
 
-		@Override
-		protected boolean canRemovePaint(ItemStack stack) {
-			return super.canRemovePaint(stack) && !WoodStorageBlockItem.isPacked(stack);
-		}
 	}
 
 	@SuppressWarnings("java:S6548") // singleton is correct here
