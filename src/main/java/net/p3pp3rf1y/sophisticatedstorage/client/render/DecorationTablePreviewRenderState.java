31c4cb98bb4e8a58d03fdee84db43c77a639ce4c
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-07-26 11:43:11 +0200

    fix: 🐛 Fixed Decoration Table preview rotation and scale

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/client/render/DecorationTablePreviewRenderState.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/client/render/DecorationTablePreviewRenderState.java
@@ -8,7 +8,7 @@ import org.joml.Matrix3x2f;
 import javax.annotation.Nullable;
 
 public record DecorationTablePreviewRenderState(TrackingItemStackRenderState itemStackRenderState, Matrix3x2f pose, @Nullable ScreenRectangle scissorArea,
-		int x0, int y0, int x1, int y1) implements PictureInPictureRenderState {
+		int x0, int y0, int x1, int y1, float xAxisRotation, float yAxisRotation) implements PictureInPictureRenderState {
 	@Override
 	public float scale() {
 		return 16.0F;
--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/client/render/DecorationTablePreviewRenderer.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/client/render/DecorationTablePreviewRenderer.java
@@ -2,6 +2,7 @@ package net.p3pp3rf1y.sophisticatedstorage.client.render;
 
 import com.mojang.blaze3d.platform.Lighting;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.render.pip.PictureInPictureRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
@@ -20,7 +21,9 @@ public class DecorationTablePreviewRenderer extends PictureInPictureRenderer<Dec
 
 	@Override
 	protected void renderToTexture(DecorationTablePreviewRenderState renderState, PoseStack poseStack) {
-		poseStack.scale(1.0F, -1.0F, -1.0F);
+		poseStack.mulPose(Axis.XN.rotationDegrees(-renderState.xAxisRotation()));
+		poseStack.mulPose(Axis.YP.rotationDegrees(-renderState.yAxisRotation()));
+		poseStack.scale(4, -4, -4);
 		TrackingItemStackRenderState itemStackRenderState = renderState.itemStackRenderState();
 		Minecraft.getInstance().gameRenderer.getLighting()
 				.setupFor(itemStackRenderState.usesBlockLight() ? Lighting.Entry.ITEMS_3D : Lighting.Entry.ITEMS_FLAT);
