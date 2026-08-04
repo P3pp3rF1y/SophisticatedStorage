74d5b02ef0aa45ebdd056dd5a42756a9c1abfce7
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-08-05 00:01:00 +0200

    fix: 🐛 Finalize Decoration Table preview rotation

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/client/render/DecorationTablePreviewRenderState.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/client/render/DecorationTablePreviewRenderState.java
@@ -4,11 +4,12 @@ import net.minecraft.client.gui.navigation.ScreenRectangle;
 import net.minecraft.client.gui.render.state.pip.PictureInPictureRenderState;
 import net.minecraft.client.renderer.item.TrackingItemStackRenderState;
 import org.joml.Matrix3x2f;
+import org.joml.Quaternionfc;
 
 import javax.annotation.Nullable;
 
 public record DecorationTablePreviewRenderState(TrackingItemStackRenderState itemStackRenderState, Matrix3x2f pose, @Nullable ScreenRectangle scissorArea,
-		int x0, int y0, int x1, int y1, float xAxisRotation, float yAxisRotation) implements PictureInPictureRenderState {
+		int x0, int y0, int x1, int y1, Quaternionfc manualParentRotation) implements PictureInPictureRenderState {
 	@Override
 	public float scale() {
 		return 16;
