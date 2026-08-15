2040f6ba06b0f748fe9b733ffd0eda7c13192bb1
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-08-15 22:54:54 +0200

    fix: 🐛 Fixed unsafe network packet handling

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/item/PaintbrushItem.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/item/PaintbrushItem.java
@@ -44,10 +44,10 @@ import java.util.*;
 
 public class PaintbrushItem extends ItemBase {
 	public static final Codec<Map<ResourceLocation, Integer>> REMAINING_PARTS_CODEC = Codec.unboundedMap(ResourceLocation.CODEC, ExtraCodecs.POSITIVE_INT);
+	private static final int MAX_REMAINING_PARTS = 64;
 
-	public static final StreamCodec<FriendlyByteBuf, Map<ResourceLocation, Integer>> REMAINING_PARTS_STREAM_CODEC = StreamCodec.of(
-			(buf, map) -> buf.writeMap(map, ResourceLocation.STREAM_CODEC, ByteBufCodecs.INT),
-			buf -> buf.readMap(ResourceLocation.STREAM_CODEC, ByteBufCodecs.INT));
+	public static final StreamCodec<FriendlyByteBuf, Map<ResourceLocation, Integer>> REMAINING_PARTS_STREAM_CODEC = ByteBufCodecs.map(HashMap::new,
+			ResourceLocation.STREAM_CODEC, ByteBufCodecs.INT, MAX_REMAINING_PARTS);
 
 	public PaintbrushItem() {
 		super(new Properties().stacksTo(1));
