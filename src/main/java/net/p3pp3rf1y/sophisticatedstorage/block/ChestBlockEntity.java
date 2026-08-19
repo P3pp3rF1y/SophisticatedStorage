d6822bcdec23540481e45949cf9b7606a94e8f92
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-08-19 13:58:57 +0200

    fix: 🐛 Fixed controller-connected double chests crashing during tier upgrades

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/block/ChestBlockEntity.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/block/ChestBlockEntity.java
@@ -436,7 +436,7 @@ public class ChestBlockEntity extends WoodStorageBlockEntity {
 
 	@Override
 	public void changeSlots(int newSlots) {
-		if (hasStorageData()) {
+		if (hasStorageData() && !isBeingUpgraded()) {
 			super.changeSlots(newSlots);
 		}
 	}
